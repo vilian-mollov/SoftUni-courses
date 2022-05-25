6d22302a05bc7ccfab5393c9ffda49c95d7b1b61
Avalon <vilian3737@gmail.com>
2022-05-25 20:47:03 +0300

    Added a file in exercise - NxNMatrix.java

---
+++ Fundamentals - Java/Methods/Exercise/N7NxNMatrix.java
@@ -0,0 +1,25 @@
+package Methods.Exercise;
+
+import java.util.Scanner;
+
+public class N7NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        matrixOfNxN(n);
+
+
+    }
+
+    private static void matrixOfNxN(int n) {
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
