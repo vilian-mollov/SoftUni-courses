6958c287f3c4cb718dcc2457cb0970b64147d143
Avalon <vilian3737@gmail.com>
2022-06-06 21:41:55 +0300

    Added a RandomizeWords.java file in new directory ObjectsAndClasses/lab

---
+++ Fundamentals - Java/ObjectsAndClasses/RandomizeWords.java
@@ -0,0 +1,36 @@
+package ObjectsAndClasses.lab;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RandomizeWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> listOfWords = Arrays.stream(scanner.nextLine().split(" "))
+                .collect(Collectors.toList());
+        Random rnd = new Random();
+        List<Integer> numbers = new ArrayList<>();
+        int countTo = listOfWords.size();
+        int count =0;
+        for (int i = 0; i < listOfWords.size(); i++) {
+            int temp = rnd.nextInt(listOfWords.size());
+            if(i==0 && numbers.isEmpty()){
+                System.out.println(listOfWords.get(temp));
+                count++;
+                numbers.add(temp);
+            }
+            if(!numbers.contains(Integer.valueOf(temp))) {
+                System.out.println(listOfWords.get(temp));
+                numbers.add(temp);
+                count++;
+            }else{
+                i= count-1;
+            }
+            if(count==countTo){
+                break;
+            }
+        }
+
+
+    }
+}
