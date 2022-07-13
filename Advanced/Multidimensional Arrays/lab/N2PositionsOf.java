b8efe4d68cacd85003ddcf65a7d9bdefd753fe4f
Avalon <vilian3737@gmail.com>
2022-07-13 10:58:06 +0300

    Refactored N2PositionsOf.java

--- Advanced/Multidimensional Arrays/lab/N2PositionsOf.java
+++ Advanced/Multidimensional Arrays/lab/N2PositionsOf.java
@@ -1,6 +1,8 @@
 package MultidimensionalArrays.lab;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class N2PositionsOf {
@@ -10,20 +12,27 @@ public class N2PositionsOf {
         int[][] matrix = readMatrix(scanner);
 
         int number = Integer.parseInt(scanner.nextLine());
-      boolean cantFoundTheNumber = true;
+       // boolean cantFoundTheNumber = true;
+        List<String> outputs = new ArrayList<>();
 
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 if(matrix[row][col] == number){
-                    System.out.println(row+" "+col);
-                    cantFoundTheNumber=false;
+                    //System.out.println(row+" "+col);
+                    outputs.add(row+" "+col);
+                    //cantFoundTheNumber=false;
                 }
             }
         }
-        if(cantFoundTheNumber){
-            System.out.println("not found");
-        }
+       String output = print(outputs);
+        System.out.println(output);
+//        if(cantFoundTheNumber){
+//            System.out.println("not found");
+//        }
 
+    }
+    public static String print(List<String> outputs){
+        return outputs.isEmpty() ?"not found":String.join(System.lineSeparator(),outputs);
     }
     public static int[][] readMatrix(Scanner scanner){
         String[] dimensions = scanner.nextLine().split("\\s+");
