41ac21ce2a61eb6099f69d43a8b5652ab0b7a44f
Avalon <vilian3737@gmail.com>
2022-07-13 10:31:13 +0300

    Added a N2PositionsOf.java

---
+++ Advanced/Multidimensional Arrays/lab/N2PositionsOf.java
@@ -0,0 +1,47 @@
+package MultidimensionalArrays.lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N2PositionsOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matrix = readMatrix(scanner);
+
+        int number = Integer.parseInt(scanner.nextLine());
+      boolean cantFoundTheNumber = true;
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if(matrix[row][col] == number){
+                    System.out.println(row+" "+col);
+                    cantFoundTheNumber=false;
+                }
+            }
+        }
+        if(cantFoundTheNumber){
+            System.out.println("not found");
+        }
+
+    }
+    public static int[][] readMatrix(Scanner scanner){
+        String[] dimensions = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            int[] arr= Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            matrix[row]=arr;
+        }
+
+
+
+        return  matrix;
+    }
+}
