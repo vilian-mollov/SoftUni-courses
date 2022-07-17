ebdb28ec260e48d217cdd6f97b62ff1470427242
Avalon <vilian3737@gmail.com>
2022-07-17 21:32:48 +0300

    Added a N9Largest3Numbers.java

---
+++ Advanced/SetsAndMapsAdvanced/lab/N9Largest3Numbers.java
@@ -0,0 +1,21 @@
+package SetsAndMapsAdvanced.lab;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class N9Largest3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println(Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                        .sorted(Comparator.reverseOrder())
+                        .limit(3)
+                        .map(String::valueOf)
+                .collect(Collectors.joining(" ")));
+
+    }
+}
