fd42022be39a4b0c608b66dac6933878cd6918de
Avalon <vilian3737@gmail.com>
2022-09-10 18:00:47 +0300

    Added ListUtils.java in Advanced/Generics

---
+++ Advanced/Generics/ListUtils.java
@@ -0,0 +1,21 @@
+package Generics;
+
+import java.util.List;
+
+public class ListUtils {
+
+
+    public static <T extends Comparable<T>> T getMax(List<T> elements){
+        return elements.stream()
+                .max(T::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("empty collection"));
+    }
+
+
+    public static <T extends Comparable<T>> T getMin(List<T> elements){
+        return elements.stream()
+                .min(T::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("empty collection"));
+    }
+
+}
