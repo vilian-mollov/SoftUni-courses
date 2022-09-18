3e94e3286869194087f016618d7e77cba54a5fd2
Avalon <vilian3737@gmail.com>
2022-09-18 11:32:50 +0300

    Recursive fibonacci with stack Updated N6RecursiveFibonacci.java

--- Advanced/StackAndQueue/exercise/N6RecursiveFibonacci.java
+++ Advanced/StackAndQueue/exercise/N6RecursiveFibonacci.java
@@ -7,29 +7,19 @@ public class N6RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque stack = new ArrayDeque<>();
 
-        if (n < 2) {
-            System.out.println(1);
-            return;
+        System.out.println(fibonacci(n,stack));
+
+    }
+
+    private static long fibonacci(int n,ArrayDeque stack) {
+        if (n <= 1) {
+            return 1;
         }
 
-        ArrayDeque<Long> stack = new ArrayDeque<>();
-
-        stack.push(0L);    // 0 element of fibonacci
-        stack.push(1L);    // 1 element of fibonacci
-
-        for (int i = 1; i < n; i++) {
-            long number1 = stack.pop();
-            long number2 = stack.pop();
-
-            stack.push(number1);
-            stack.push(number1 + number2);
-        }
-
-
-        System.out.println(stack.peek());  // n-th element of fibonacci
-
-
+        stack.push(fibonacci(n - 1,stack) + fibonacci(n - 2,stack));
+        return (long)stack.pop();
     }
 
 }
