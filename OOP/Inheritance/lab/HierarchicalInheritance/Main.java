72c6a9f01eaf093e602743611c7e6d89cc732590
Avalon <vilian3737@gmail.com>
2022-10-02 14:25:49 +0300

    Added HierarchicalInheritance Animal.java Dog.java Cat.java Main.java

---
+++ OOP/Inheritance/lab/HierarchicalInheritance/Main.java
@@ -0,0 +1,13 @@
+package Inheritance.lab.HierarchicalInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+
+    }
+}
