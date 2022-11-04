033fed7c1161158a8258d180c413d7b60808894e
Avalon <vilian3737@gmail.com>
2022-11-04 18:40:12 +0200

    Added European.java class

---
+++ OOP/InterfacesAndAbstraction/lab/SayHello/European.java
@@ -0,0 +1,11 @@
+package InterfacesAndAbstraction.lab.SayHello;
+
+public class European extends BasePerson {
+
+    private String name;
+
+    public European(String name) {
+        super(name);
+    }
+
+}
