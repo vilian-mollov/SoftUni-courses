2eaf8bccb2c83c81904a0d7679d7c7e8a9da587d
Avalon <vilian3737@gmail.com>
2022-12-02 19:13:28 +0200

    Finished InputInterpreter.java

--- OOP/Polymorphism/exercise/Calculator/InputInterpreter.java
+++ OOP/Polymorphism/exercise/Calculator/InputInterpreter.java
@@ -3,21 +3,28 @@ package Polymorphism.exercise.Calculator;
 public class InputInterpreter {
     private CalculationEngine engine;
 
-    public InputInterpreter(CalculationEngine engine){
+    public InputInterpreter(CalculationEngine engine) {
         this.engine = engine;
     }
 
     public boolean interpret(String input) {
         try {
             engine.pushNumber(Integer.parseInt(input));
-        }catch (Exception ex){
+        } catch (Exception ex) {
             engine.pushOperation(this.getOperation(input));
         }
         return true;
     }
+
     public Operation getOperation(String operation) {
         if (operation.equals("*")) {
             return new MultiplicationOperation();
+        } else if (operation.equals("/")) {
+            return new DivisionOperation();
+        } else if (operation.equals("ms")) {
+            return new MemorySaveOperation();
+        } else if (operation.equals("mr")) {
+            return new MemoryRecall();
         }
 
         return null;
