e982dcd0c5c1c5a99d8c7a5c29ffed1596a32506
Avalon <vilian3737@gmail.com>
2022-12-02 19:12:47 +0200

    Added MemoryRecall.java

---
+++ OOP/Polymorphism/exercise/Calculator/MemoryRecall.java
@@ -0,0 +1,21 @@
+package Polymorphism.exercise.Calculator;
+
+public class MemoryRecall implements Operation{
+    @Override
+    @Deprecated
+    public void addOperand(int operand) {
+
+    }
+
+    @Override
+    @Deprecated
+    public int getResult() {
+        return 0;
+    }
+
+    @Override
+    @Deprecated
+    public boolean isCompleted() {
+        return false;
+    }
+}
