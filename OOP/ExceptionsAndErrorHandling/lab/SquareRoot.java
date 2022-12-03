d0c34ef069097b1a89417a002e21f4a01ac0b39f
Avalon <vilian3737@gmail.com>
2022-12-03 18:53:29 +0200

    Added new task SquareRoot.java

---
+++ OOP/ExceptionsAndErrorHandling/lab/SquareRoot.java
@@ -0,0 +1,29 @@
+package ExceptionsAndErrorHandling.lab;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            double n = Double.parseDouble(scanner.nextLine());
+
+            if (n < 0) {
+                throw new NumberFormatException("Invalid");
+            }
+
+            double squareRoot = Math.sqrt(n);
+            System.out.printf("%.2f%n", squareRoot);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid");
+
+        } finally {
+            System.out.println("Goodbye");
+        }
+
+
+    }
+}
