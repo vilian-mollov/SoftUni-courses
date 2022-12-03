1430f75c3b99bcef6d7c2469a840014137d90a80
Avalon <vilian3737@gmail.com>
2022-12-03 12:22:24 +0200

    Added SOLID exercise skeleton and documentation

---
+++ OOP/SOLID/exercise/CalorieCalculator.java
@@ -0,0 +1,50 @@
+package SOLID.exercise;
+
+import SOLID.exercise.products.Chocolate;
+import SOLID.exercise.products.Coke;
+import SOLID.exercise.products.Lemonade;
+
+import java.util.List;
+
+public class CalorieCalculator {
+
+    private static final String SUM = "Sum: %f";
+    private static final String AVERAGE = "Average: %f";
+
+    public CalorieCalculator() {
+    }
+
+    public double sum(List<Object> products) {
+        double sum = 0;
+
+        for (Object product : products) {
+            if (product instanceof Coke) {
+                double grams = ((Coke) product).getMilliliters() * Coke.DENSITY;
+                sum += (Coke.CALORIES_PER_100_GRAMS / 100) * grams;
+            }
+
+            if (product instanceof Lemonade) {
+                double grams = ((Lemonade) product).getMilliliters() * Lemonade.DENSITY;
+                sum += (Lemonade.CALORIES_PER_100_GRAMS / 100) * grams;
+            }
+
+            if (product instanceof Chocolate) {
+                sum += (Chocolate.CALORIES_PER_100_GRAMS / 100) * ((Chocolate) product).getGrams();
+            }
+        }
+
+        return sum;
+    }
+
+    public double average(List<Object> products) {
+        return sum(products) / products.size();
+    }
+
+    public void printSum(List<Object> products) {
+        System.out.printf((SUM) + "%n", sum(products));
+    }
+
+    public void printAverage(List<Object> products) {
+        System.out.printf((AVERAGE) + "%n", average(products));
+    }
+}
---
+++ OOP/SOLID/exercise/Main.java
@@ -0,0 +1,7 @@
+package SOLID.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
