a80c4ab98a14ec051f92ecc3652bd2908ec20002
Avalon <vilian3737@gmail.com>
2022-12-06 08:43:16 +0200

    Added tests in p02_ExtendedDatabase/DatabaseTest.java tested findById() method

--- Unit-Testing-Exercise/src/test/java/p02_ExtendedDatabase/DatabaseTest.java
+++ Unit-Testing-Exercise/src/test/java/p02_ExtendedDatabase/DatabaseTest.java
@@ -9,12 +9,18 @@ import static org.junit.Assert.*;
 
 public class DatabaseTest {
 
+    private static final Person person1 = new Person(14, "Toni");
+    private static final Person person2 = new Person(15, "Jakarcho");
+    private static final Person person3 = new Person(24, "Alukardcho");
     private static Person[] people;
     private static Database database;
 
     @Before
     public void setUp() throws OperationNotSupportedException {
-        people = new Person[10];
+        people = new Person[3];
+        people[0] = person1;
+        people[1] = person2;
+        people[2] = person3;
         database = new Database(people);
     }
 
@@ -33,6 +39,38 @@ public class DatabaseTest {
     public void test_Database_Constructor_ThrowsException_When_LessThan_1_Element_InParameter() throws OperationNotSupportedException {
         people = new Person[0];
         database = new Database(people);
+    }
+
+    @Test
+    public void test_Database_FindByUserName_ValidCase() throws OperationNotSupportedException {
+        Person actualPerson = database.findByUsername("Jakarcho");
+        assertEquals(person2,actualPerson);
+    }
+
+    @Test
+    public void test_Database_FindByUserName_When_NoUser_Is_Presented_ThrowsException_O_N_S_Exception() {
+    }
+
+    @Test
+    public void test_Database_FindByUserName_When_UsernameParameter_Is_Null_ThrowsException_O_N_S_Exception() {
+    }
+
+    @Test
+    public void test_Database_FindById_ValidCase() throws OperationNotSupportedException {
+        Person actualPerson = database.findById(15);
+        assertEquals(15, actualPerson.getId());
+    }
+
+    @Test
+    public void test_Database_FindById_When_NoUser_IsPresented_ByThis_Id_Throws_Operation_N_S_Exception() {
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void test_Database_FindById_When_MoreThanOne_User_Is_With_This_Id_Throws_O_N_S_Exception() throws OperationNotSupportedException {
+        Person duplicate = new Person(15, "NotJakarcho");
+        database.add(duplicate);
+
+        database.findById(15);
     }
 
 
