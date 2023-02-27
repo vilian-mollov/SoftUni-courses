44170f0259edbe9ab8d07c1a47852518ef38d4aa
Avalon <vilian3737@gmail.com>
2023-02-27 14:40:20 +0200

    Added new task N3_excellentGrade.js

---
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/JS_Syntax_Fundamentals/N3_excellentGrade.js
@@ -0,0 +1,11 @@
+let grade = 5.50;
+
+function checkGrade(grade){
+    if (grade >= 5.50) {
+        return "Excellent";
+    } else {
+        return "Not excellent";
+    }
+}
+
+console.log(checkGrade(grade));
