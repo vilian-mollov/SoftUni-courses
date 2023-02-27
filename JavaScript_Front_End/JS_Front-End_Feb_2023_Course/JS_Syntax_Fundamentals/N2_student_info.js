4198def88a9643421c3ada3ef2e9d7c4218e18d2
Avalon <vilian3737@gmail.com>
2023-02-27 14:37:54 +0200

    Added

---
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/JS_Syntax_Fundamentals/N2_student_info.js
@@ -0,0 +1,10 @@
+let name = "Jhon";
+let age = 23;
+let grade = 5.896541556;
+
+
+function printInfo(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+printInfo(name, age, grade)
