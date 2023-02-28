d064fa0487accc9fcee1604ffe5766bdee16d72c
Avalon <vilian3737@gmail.com>
2023-02-28 13:04:51 +0200

    Added new task  N14_substring.js

---
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/JS_Syntax_Fundamentals/N14_substring.js
@@ -0,0 +1,10 @@
+let text = 'ASentence';
+let start = 1;
+let end = 8;
+
+function substringFormText (text, start, end) {
+    return text.substring(start, start + end);
+}
+
+
+console.log(substringFormText(text, start, end));
