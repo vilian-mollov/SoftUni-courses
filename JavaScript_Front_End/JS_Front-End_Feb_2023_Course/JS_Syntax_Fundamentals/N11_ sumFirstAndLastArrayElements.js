b46bd5e30b4194b5856f6760ae9ae52cb7467bcb
Avalon <vilian3737@gmail.com>
2023-02-28 11:04:05 +0200

    Added new task N11_ sumFirstAndLastArrayElements.js

---
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/JS_Syntax_Fundamentals/N11_ sumFirstAndLastArrayElements.js
@@ -0,0 +1,11 @@
+let arrOne = [15,23,77,4];
+let arrTwo = [45,21,32,7];
+
+arrOne
+
+function sumFirstAndLastEl(array) {
+    return array[0] + array[array.length-1];
+}
+
+
+console.log(sumFirstAndLastEl(arrTwo));
