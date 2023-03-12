a38546a6688a609ec5ada3b91df0d1280655e00a
Avalon <vilian3737@gmail.com>
2023-03-12 11:01:31 +0200

    Refactered N7-meetings.js

--- JavaScript_Front_End/JS_Front-End_Feb_2023_Course/Objects_and_Classes/lab/N7-meetings.js
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/Objects_and_Classes/lab/N7-meetings.js
@@ -1,23 +1,23 @@
 function manageMeetings(input) {
 
-    let assocArr = {};
+    let meetingSchedule = {};
 
     input.forEach(couple => {
         let tokens = couple.split(" ");
         let [day, name] = tokens;
 
-        if(assocArr.hasOwnProperty(day)){
+        if(meetingSchedule.hasOwnProperty(day)){
             console.log(`Conflict on ${day}!`);
         }else {
-            assocArr[day] = name;
+            meetingSchedule[day] = name;
             console.log(`Scheduled for ${day}`);
         }
 
     });
 
 
-    for (const key in assocArr) {
-        console.log(`${key} -> ${assocArr[key]}`);
+    for (const key in meetingSchedule) {
+        console.log(`${key} -> ${meetingSchedule[key]}`);
     }
 
 }
