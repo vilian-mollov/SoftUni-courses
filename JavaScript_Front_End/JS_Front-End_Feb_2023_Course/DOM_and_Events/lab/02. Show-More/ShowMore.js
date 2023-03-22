8f91c58c11df1dc79e14a5dbce54f31024989008
Avalon <vilian3737@gmail.com>
2023-03-22 18:33:53 +0200

    Added new task 02. Show-More at JavaScript_Front_End/JS_Front-End_Feb_2023_Course/DOM_and_Events/lab>

---
+++ JavaScript_Front_End/JS_Front-End_Feb_2023_Course/DOM_and_Events/lab/02. Show-More/ShowMore.js
@@ -0,0 +1,10 @@
+function showText() {
+   const anchor = document.getElementById('more');
+   const text = document.getElementById('text');
+   const lor = document.getElementById('lor');
+   const lors = document.querySelectorAll
+
+   text.style.display = 'inline';
+   lor.style.display = 'inline';
+   anchor.style.display = 'none';
+}
