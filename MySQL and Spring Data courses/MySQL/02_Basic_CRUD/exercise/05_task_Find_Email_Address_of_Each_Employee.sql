92dfabbf011720aa42d909c29fa96a8e2ad57f24
Avalon <vilian3737@gmail.com>
2023-05-19 22:23:29 +0300

    Added new task in 02_Basic_CRUD/exercise/05_task_Find_Email_Address_of_Each_Employee.sql

---
+++ MySQL and Spring Data courses/MySQL/02_Basic_CRUD/exercise/05_task_Find_Email_Address_of_Each_Employee.sql
@@ -0,0 +1,9 @@
+USE soft_uni;
+
+-- Write a SQL query to find the email address of each employee. (by his first and last name). Consider that the email 
+-- domain is softuni.bg. Emails should look like "John.Doe@softuni.bg". The produced column should be named "full_ 
+-- email_address". Submit your query statements as Prepare DB & run queries.
+
+SELECT concat( first_name, '.', last_name, '@softuni.bg' ) AS `full_email_address`
+FROM employees; 
+
