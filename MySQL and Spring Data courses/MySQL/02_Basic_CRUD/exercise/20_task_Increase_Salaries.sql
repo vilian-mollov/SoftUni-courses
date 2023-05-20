5d20b5b21eeabfb18a30857b246bf4b7ce68069b
Avalon <vilian3737@gmail.com>
2023-05-20 11:32:16 +0300

    Added new task in 02_Basic_CRUD/exercise/20_task_Increase_Salaries.sql

---
+++ MySQL and Spring Data courses/MySQL/02_Basic_CRUD/exercise/20_task_Increase_Salaries.sql
@@ -0,0 +1,10 @@
+USE soft_uni;
+
+-- Write a SQL query to increase salaries of all employees that are in the Engineering, Tool Design, Marketing or 
+-- Information Services department by 12%. Then select Salaries column from the Employees table.
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id = 1 OR department_id = 2 OR department_id = 4 OR department_id = 11;
+
+
+SELECT salary FROM employees;
