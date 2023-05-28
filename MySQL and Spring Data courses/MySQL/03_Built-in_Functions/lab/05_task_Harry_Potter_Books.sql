b574b3151b183d124532442cca467133c2b995d3
Avalon <vilian3737@gmail.com>
2023-05-28 22:59:38 +0300

    Updated task in 03_Built-in_Functions/lab/05_task_Harry_Potter_Books.sql

--- MySQL and Spring Data courses/MySQL/03_Built-in_Functions/lab/05_task_Harry_Potter_Books.sql
+++ MySQL and Spring Data courses/MySQL/03_Built-in_Functions/lab/05_task_Harry_Potter_Books.sql
@@ -2,6 +2,6 @@ USE book_library;
 
 -- Write a SQL query to retrieve titles of all the Harry Potter books. Order the information by id.
 
-SELECT * FROM books
+SELECT title FROM books
 WHERE title REGEXP '^Harry Potter'
 ORDER BY id;
