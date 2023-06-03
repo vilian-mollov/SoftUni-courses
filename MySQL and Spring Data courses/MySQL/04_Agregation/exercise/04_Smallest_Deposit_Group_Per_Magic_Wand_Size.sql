05656314e11b66b94a76b2b593238fa641689ab8
Avalon <vilian3737@gmail.com>
2023-06-03 12:21:01 +0300

    Added task in 04_Agregation/exercise/04_Smallest_Deposit_Group_Per_Magic_Wand_Size.sql

---
+++ MySQL and Spring Data courses/MySQL/04_Agregation/exercise/04_Smallest_Deposit_Group_Per_Magic_Wand_Size.sql
@@ -0,0 +1,8 @@
+USE gringotts;
+
+-- Select the deposit group with the lowest average wand size
+
+SELECT deposit_group FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY AVG(magic_wand_size)
+LIMIT 1;
