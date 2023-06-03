0685272c41467b331ee60d4e5c545564733f8e78
Avalon <vilian3737@gmail.com>
2023-06-03 12:20:09 +0300

    Added task in 04_Agregation/exercise/01_ Records_Count.sql

---
+++ MySQL and Spring Data courses/MySQL/04_Agregation/exercise/01_ Records_Count.sql
@@ -0,0 +1,9 @@
+USE gringotts;
+
+-- Mr. Bodrog is a greedy small goblin. His most precious possession is a small database of the deposits in 
+-- the wizard's world. Mr. Bodrog wants you to send him some reports.
+-- Get familiar with the gringotts database. You will use it in the assignments below.
+
+-- Import the database and send the total count of records to Mr. Bodrog. Make sure nothing got lost.
+
+SELECT COUNT(*) FROM wizzard_deposits;
