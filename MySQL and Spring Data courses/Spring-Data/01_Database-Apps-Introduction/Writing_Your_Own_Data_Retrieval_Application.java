89b2ffc06e318a8a2241568237b4eb3d174afaf7
Avalon <vilian3737@gmail.com>
2023-06-25 20:14:47 +0300

    Added task to 01_Database-Apps-Introduction/Writing_Your_Own_Data_Retrieval_Application.java

---
+++ MySQL and Spring Data courses/Spring-Data/01_Database-Apps-Introduction/Writing_Your_Own_Data_Retrieval_Application.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Writing_Your_Own_Data_Retrieval_Application {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "*********");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+
+        PreparedStatement preparedStatement =
+                connection
+                        .prepareStatement("SELECT count(*) AS games, user_name, first_name, last_name " +
+                                              "FROM users_games " +
+                                              "join users on user_id = users.id " +
+                                              "where user_name = ? " +
+                                              "group by users.id;");
+
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println();
+
+        preparedStatement.setString(1, username);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        boolean hasRecord = false;
+
+
+        while (resultSet.next()) {
+
+            hasRecord = true;
+
+            System.out.println("User: " + resultSet.getString("user_name") + "\n"
+                    + resultSet.getString("first_name") + " " + resultSet.getString("last_name")
+                    + " has played " + resultSet.getInt("games") + " games");
+
+            System.out.println();
+
+        }
+
+
+        if(!hasRecord) {
+
+            System.out.println("No such user exists");
+
+        }
+
+
+    }
+}
