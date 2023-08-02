451c347ccd594b50b570798435d89c7e41836b99
Avalon <vilian3737@gmail.com>
2023-08-02 22:56:02 +0300

    Refactored HomeController

--- MySQL and Spring Data courses/Spring-Data/10_Workshop_MVC_Project/mvc-project/src/main/java/com/softuni/mvcproject/controllers/HomeController.java
+++ MySQL and Spring Data courses/Spring-Data/10_Workshop_MVC_Project/mvc-project/src/main/java/com/softuni/mvcproject/controllers/HomeController.java
@@ -2,12 +2,22 @@ package com.softuni.mvcproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("person", "Alocard");
+
+        return modelAndView;
     }
 }
