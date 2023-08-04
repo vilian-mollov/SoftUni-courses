74d2d44b6912aa93c46ac582fa09f91a6010c47d
Avalon <vilian3737@gmail.com>
2023-08-04 20:55:31 +0300

    Add User entity and User repository

---
+++ MySQL and Spring Data courses/Spring-Data/10_Workshop_MVC_Project/mvc-project/src/main/java/com/softuni/mvcproject/entities/User.java
@@ -0,0 +1,77 @@
+package com.softuni.mvcproject.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ MySQL and Spring Data courses/Spring-Data/10_Workshop_MVC_Project/mvc-project/src/main/java/com/softuni/mvcproject/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.softuni.mvcproject.repositories;
+
+import com.softuni.mvcproject.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+}
